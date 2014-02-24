49f143f9c00572879aa7ec18c4b37b28cdf7ce2b
Kevin Wang <kevin@cnkvha.com>
2014-02-24 18:07:36 +0800

    Basic things

---
+++ BukkitPE/BukkitPEServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BukkitPE
+{
+    class BukkitPEServer
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
